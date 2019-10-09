484a3d5535e2d457ed5bb3f9a06213b9e45d05c2
Adam Mathieson <mathieson1222@gmail.com>
2019-10-09 15:48:39 -0700

    cleans up the code

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python
 
-# import argparse
+import argparse
 
 def find_max_profit(prices):
   profit_list = []
   profit = None
+
   for i in range(len(prices)):
     if len(prices[i+1:]) > 0:
-      print(prices[i], prices[i+1:])
+      # print(prices[i], prices[i+1:])
       for j in prices[i+1:]:
         profit_list.append(j - prices[i])
     profit_list.sort()
-    print("return", profit_list[-1])
+    # print("return", profit_list[-1])
     profit = profit_list[-1]
-    print(profit)
+    # print(profit)
+
   return profit
 
 
   
 
-# if __name__ == '__main__':
-#   # This is just some code to accept inputs from the command line
-#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
-#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-#   args = parser.parse_args()
+if __name__ == '__main__':
+  # This is just some code to accept inputs from the command line
+  parser = argparse.ArgumentParser(description='Find max profit from prices.')
+  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+  args = parser.parse_args()
 
-#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
-
-test = [10, 7, 5, 8, 11, 9]
-
-find_max_profit(test)
+  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
