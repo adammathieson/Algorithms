bddf938b549639c06db9f79726937e6ace06f200
Adam Mathieson <mathieson1222@gmail.com>
2019-10-13 23:57:23 -0700

    rpc passing test; non-default agruments cant come before default; must use + for concatenate the choice as list

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,23 +2,49 @@
 
 import sys
 
-def add_round(arr):
- 
-  choice = ['rock', 'paper', 'scissors']
+# def add_round(arr):
 
+#   choice = ['rock', 'paper', 'scissors']
 
-  return arr
+#   if len(arr) < 3:
+#     for c in choice:
+#       arr.append([c])
+#       arr.append([c])
+#       arr.append([c])
+#   else:
+#     pass
+    # arr += arr.copy() + arr.copy()
+    # arr.sort(
+    # arr *= 3
+    # arr.sort()
+    # for i in arr:
+    #   i *= 3
+
+    # for i in arr:
+    #   for c in choice:
+    #     i.append(c)
+    # arrA = arr.copy()
+    # arr += arrA * 2
+    # for i in arr:
+    #   print(i)
+    #   i
+
+  # return arr
 
 
 def rock_paper_scissors(n):
+  choice = ['rock', 'paper', 'scissors']
   plays = []
-  round = n
 
-  while round > 0:
-    add_round(plays)
-    print(round)
-    round -= 1
+  def add_round(n, play=[]):
+    if n == 0:
+      plays.append(play)
+      return
+    for c in choice:
+      # print(c)
+      add_round(n-1, play + [c])
       
+  add_round(n, [])
   return plays
 
-print(rock_paper_scissors(2))
+print(rock_paper_scissors(3))
